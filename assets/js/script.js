745a597b95c8cb580b2173264621f7db049bbae8
KWensmann <kurtwensmann@gmail.com>
2023-10-08 07:38:18 -0500

    finished coding quiz - all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,38 @@
 
 // these select where in the html our objects are going
 var startButton = document.querySelector("#start-btn");
-var questions = [{question: "?", answer: "1", choices: ["1", "2", "3", "4"]}, {question: "?2", answer: "2.2", choices: ["1.1", "2.2", "3.3", "4.4"]}];
+var questions = [
+  {question: "Which of the follow is not used in JavaScript?", 
+  answer: "<header>", 
+  choices: ["textContent", "createElement", "querySelector", "<header>"]
+  },
 
-var resultsEl = document.querySelector("#scoreboard");
-var questionChoices = document.querySelector("#questions");
-var questionTitle = document.querySelector("#question-title");
+  {question: "Inside which HTML element do we put the JavaScript?", 
+  answer: "<script>", 
+  choices: ["<javascript>", "<script>", "<js>", "<header>"]
+  },
+
+  {question: "How do you write 'Hello World' in an alert box?", 
+  answer: "alert('Hello World!'", 
+  choices: ["alertBox('Hello World!')", "msgBox('Hello World!')", "alert('Hello World!'", "msg('Hello World!')"]
+},
+
+{question: "How to write an IF statement in JavaScript?", 
+answer: "if (i == 5)", 
+choices: ["if i = 5", "if (i == 5)", "if i == 5 then", "if i = 5 then"]
+} 
+];
+
+var highScoresEl = document.querySelector("#scoreboard");
+var resultsEl = document.querySelector("#results");
+var listEl = document.getElementById("scoreboard-list");
 var timeEl = document.querySelector("#timer");
-var secondsLeft = 75;
+var submitBtnEl;
+
+
+var scoreboard = [];
+var questionChoices = document.querySelector("#questions");
+var secondsLeft = 60;
 var timerInterval;
 var choiceButton = document.querySelector(".choice-btns");
 var wins = 0;
@@ -52,6 +77,9 @@ function startGame() {
     setTime(); //run timer
     startButton.style.opacity = "0";
     displayQ(); //display question
+    if (submitBtnEl) {
+      submitBtnEl.disabled = false; //enalbles submit button
+    }
   } else {
     startButton.textContent = "Start";
     currentQ = 0;
@@ -60,68 +88,148 @@ function startGame() {
     losses = 0;
     resultsEl.style.opacity = "0";
     timeEl.textContent = `Time: ${secondsLeft}`;
-    resultsEl.innerHTML = ""
-  }}
+    resultsEl.innerHTML = "";
+    listEl.innerHTML = "";    ///clears previous list
+}}
+
+function timeout() {
+
+}
+
+var i = document.querySelector(".is-wrong-or");
+i.style.display = "none";
+
+function chooseAnswer(e) {
+  if (e.target.matches("button")) {
+    var btn = e.target //e = the click, target = object
+    if (btn.textContent === questions[currentQ].answer) {
+      wins++;
+      i.textContent ="Correct!";
+      i.style.display = "block";
+    } else {
+      losses++;
+      secondsLeft -= 15; //lose 15 seconds if you get the answer wrong
+      i.textContent = "Incorrect";
+      i.style.display = "block";
+    }
+    
+    setTimeout(function() {
+      i.style.display = "none"
+    }, 1000)
+    
+    //move to next question
+    currentQ++;
+    //check if there are more questions
+    if (currentQ < questions.length) {
+      displayQ();
+    } else {
+      questionChoices.innerHTML = ""; //clears out question
+      results();
+      submitBtnEl.disabled = false;
+    }
+  }
+}
   
 function displayQ() {
   //clear previous question and choices
   questionChoices.innerHTML = ""
   //display the current question
-  questionTitle.textContent = questions[currentQ].question;
+  var questionTitleEL = document.createElement("h2");
+  questionTitleEL.textContent = questions[currentQ].question;
+  questionChoices.appendChild(questionTitleEL);
+
   for (var i = 0; i < questions[currentQ].choices.length; i++) {
     choiceButton = document.createElement("button");
     choiceButton.textContent = questions[currentQ].choices[i];
     choiceButton.setAttribute("class", "choice-btns");
     questionChoices.appendChild(choiceButton);
-    // console.log(choiceButton);
-
-    //create a closure to capture value of choiceButton so create event listener for each button
-    (function(button) {
-      choiceButton.addEventListener("click", function() {
-        console.log(button.textContent)
-        if (button.textContent === questions[currentQ].answer) {
-          wins++;
-          console.log(wins);
-        } else {
-          losses++;
-          secondsLeft -= 10; //lose 10 seconds if you get the answer wrong
-          console.log(losses);
-        }
-        //move to next question
-        currentQ++;
-        //check if there are more questions
-        if (currentQ < questions.length) {
-          displayQ();
-        } else {
-          questionChoices.innerHTML = ""; //clears out question
-          results();
-        }
-      });
-    }) (choiceButton);  //immediately invoke closure function with choiceButton
   }
- 
-  console.log(currentQ);
+}
+function results() {
+  resultsEl.style.opacity = "1";
+  //create title
+  resultsH2 = document.createElement("h2");
+  resultsH2.textContent = "Results";
+  resultsEl.appendChild(resultsH2);
+  //create results div
+  resultResults = document.createElement("p");
+  resultResults.setAttribute("class", "ptag")
+  let x = Math.max(secondsLeft, 0); //lowest possible score is 0
+  resultResults.textContent = `You got ${wins} out of ${questions.length} correct and a score of ${x}.  Good job!`;
+  resultsEl.appendChild(resultResults);
+  //create submit form
+  submitEL = document.createElement("form");
+  resultsEl.appendChild(submitEL);
+  //create input for form
+  inputEl = document.createElement("input");
+  inputEl.setAttribute("id", "user");
+  inputEl.setAttribute("type", "text");
+  inputEl.setAttribute("placeholder", "Initials");
+  submitEL.appendChild(inputEl);
+  //create submit button for form
+  submitBtnEl = document.createElement("input");
+  submitBtnEl.setAttribute("type", "submit");
+  submitBtnEl.setAttribute("value", "Submit");
+  submitBtnEl.setAttribute("id", "submit-btn");
+  submitEL.appendChild(submitBtnEl);
+  submitBtnEl.disabled = true; //disable after submiting
+}
+  
+function storeUser() {
+  // Stringify and set key in localStorage to scoreboard array
+  localStorage.setItem("scoreboard", JSON.stringify(userObject));
 }
 
-  function results() {
-    resultsEl.style.opacity = "1";
-    scoreboardH2 = document.createElement("h2");
-    scoreboardH2.textContent = "Results";
-    resultsEl.appendChild(scoreboardH2);
-    scoreboardResults = document.createElement("p");
-    scoreboardResults.textContent = `You got ${wins} out of ${questions.length} correct.  Good job!`;
-    resultsEl.appendChild(scoreboardResults);
+
+function updateScoreboard(e) {
+  e.preventDefault();
+  var score = secondsLeft;
+  var initials = document.getElementById("user").value;
+  if (e.target.matches("form")) {///.matches is looking for a tag el
+    var userObject = {
+      user: initials,
+      userScore: score
+    };
+    scoreboard.push(userObject);
+    localStorage.setItem("users-scores", JSON.stringify(scoreboard));
+    // return submitEl.style.opacity = "0";//doesnt work//////
   }
+}
+
+var scoresDisplayed = false;  //var to toggle 
+
+function displayHighScores(){
+  if (scoresDisplayed) {
+    listEl.innerHTML = "";
+    scoresDisplayed = false
+  } else {
+    scoreboard = JSON.parse(localStorage.getItem("users-scores")) || [];
+    scoreboard.sort((a,b) => b.userScore - a.userScore);
+    listEl.innerHTML = ""
+    for (var i = 0; i < scoreboard.length; i++) {
+      var liEl = document.createElement("li");
+      liEl.textContent = `User: ${scoreboard[i].user} score: ${scoreboard[i].userScore}`; 
+      listEl.appendChild(liEl);
+    }
+    scoresDisplayed = true;
+  }
+}
+
+// Get stored scoreboard from localStorage
+function init() {
+  scoreboard = JSON.parse(localStorage.getItem("users-scores")) || [];
+  console.log(scoreboard)
+}
+init();
+
+
+highScoresEl.addEventListener("click", displayHighScores);  //{once: true}
+startButton.addEventListener("click", startGame);
+questionChoices.addEventListener("click", chooseAnswer)
+resultsEl.addEventListener("submit", updateScoreboard);
+/// TO DO: CREATE highscores in local storage-- high score = timeLeft when last question answered
+/// TO DO: CREATE div element with timer that display correct or not
+
+
   
-  startButton.addEventListener("click", startGame);
-  
-  /// TO DO: CREATE highscores in local storage
-  /// TO DO: CREATE div element with timer that display correct or not
-  
-    
- 
-
-
-
-
-//web api activity 19 look at to control clicks inside of box
+  //web api activity 19 look at to control clicks inside of box
